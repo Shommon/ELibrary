5cf217e596d00dd029d4c25479a95a3fbcdb81c1
Shommon <stai2@gmu.edu>
2024-03-15 16:43:55 -0400

    Created: Sections for Header, Body, and Footer Created: Dialog box with forms for submitting book information Created: Book Container for book objects Created: function for opening and closing dialog Created: functions for creating book objects, storing their values in an array

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,21 @@
+const dialog = document.getElementById("addbookdialog");
+const closeBtn = dialog.querySelector("#close");
+
+
 const myLibrary = [];
+
+function showDialog() {
+    dialog.style.display = 'flex';
+    dialog.showModal();
+
+}
+
+closeBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    dialog.style.display = 'none';
+    dialog.close();
+})
+
 
 let Book = function(title,author,pages) {
     this.title = title;
@@ -8,10 +25,17 @@ let Book = function(title,author,pages) {
 
 let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages')
 
+
+
 function addBookToLibrary(book) {
     myLibrary.push(book)
 }
 
+
+
+
 addBookToLibrary(theHobbit)
 console.log(myLibrary[0].title)
+
+
 
